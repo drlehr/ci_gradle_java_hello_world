9e62a8cd82937810fcc52a4195b08a7c95f1b8c3
Ted Lehr <t_l81@txstate.edu>
2020-10-15 14:19:07 -0500

    first commit

---
+++ src/test/java/hello/TestGreeter.java
@@ -0,0 +1,36 @@
+package hello;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+// Change the comment again to kick off a build
+// This change is made originally on the ted2branch
+
+public class TestGreeter {
+
+   private Greeter g = new Greeter();
+
+   @Test
+   @DisplayName("Test for Empty Name")
+   public void testGreeterEmpty() 
+
+   {
+      assertEquals(g.getName(),"");
+      assertEquals(g.sayHello(),"Hello!");
+   }
+
+
+
+   @Test
+   @DisplayName("Test for Name='World'")
+   public void testGreeter() 
+   {
+
+      g.setName("World");
+      assertEquals(g.getName(),"World");
+      assertEquals(g.sayHello(),"Hello World!");
+   }
+
+}
